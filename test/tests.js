0fd2122d5906fccbd7f19db900010656660900c2
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2015-12-23 10:46:46 +0100

    implemented rabbitmq pub/sub

--- test/tests.js
+++ test/tests.js
@@ -3,13 +3,9 @@
 const SqrLib = require('../lib/index.js');
 const common = require('../lib/common.js');
 const q = require('q');
-const main = [{
-  method: 'publish',
-}, {
-  method: 'subscribe',
-}];
 
 function Tests() {
+
   it('produce', function(done) {
     const sqr = new SqrLib(this.provider);
     sqr.produce({
@@ -23,6 +19,7 @@ function Tests() {
         done(err);
       });
   });
+
   it('consume', function(done) {
     this.timeout(10000);
     const sqr = new SqrLib(this.provider);
@@ -38,24 +35,47 @@ function Tests() {
     }
     sqr.consume({queue: 'test', fct: fct})
       .then(function(data) {
-        console.log(data);
         done();
       }, function(err) {
         done(err);
       });
   });
-  main.forEach(function(e) {
-    it(e.method, function(done) {
-      const sqr = new SqrLib(this.provider);
-      sqr[e.method](this.id)
-        .then(function(data) {
-          console.log(data, '\n');
-          done();
-        }, function(err) {
-          done(err);
-        });
+
+  it('subscribe', function(done) {
+    this.timeout(10000);
+    const sqr = new SqrLib(this.provider);
+    function fct(msg) {
+      console.log('\nreceived subscribed msg: ', common.bufferToJSON(msg.content));
+    }
+    sqr.subscribe({
+      ex: 'test_ex',
+      key: 'test_key',
+      fct: fct,
+    }).then(function() {
+      done();
+    }, function(err) {
+      done(err);
     });
   });
+
+  it('publish', function(done) {
+    const sqr = new SqrLib(this.provider);
+    const json = {
+      id: '123',
+      status: 'ok',
+      description: 'simple test',
+    };
+    sqr.publish({
+      ex: 'test_ex',
+      key: 'test_key',
+      json: json,
+    }).then(function() {
+        done();
+    }, function(err) {
+      done(err);
+    });
+  });
+
 }
 
 module.exports = Tests;
