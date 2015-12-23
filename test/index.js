eb8e265122e495c6fa2293f469159ed5307169af
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2015-12-23 15:52:23 +0100

    added uri param to mq provider

--- test/index.js
+++ test/index.js
@@ -4,14 +4,18 @@ const SqrLib = require('../lib/index.js');
 const tests = require('./tests.js');
 const main = [{
   provider: 'rabbitMQProvider',
+  params: {
+    uri: 'amqp://localhost',
+  },
 }, {
   provider: 'wampKueProvider',
+  params: {},
 }];
 
 main.forEach(function(e) {
   describe(e.provider, function() {
     before(function() {
-      this.provider = new SqrLib[e.provider]();
+      this.provider = new SqrLib[e.provider](e.params);
       this.id = e.provider;
     });
     tests();
