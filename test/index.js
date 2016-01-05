56038149f7ca689d2279d55e353b7c9283c94d9b
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-01-05 15:34:51 +0100

    added Instantiation tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const assert = require('chai').assert;
 const SqrLib = require('../lib/index.js');
+
 const tests = require('./tests.js');
 const main = [{
   provider: 'rabbitMQProvider',
@@ -14,6 +16,24 @@ const main = [{
     realm: 'realm1',
   },
 }];
+
+describe('Instantiation', function() {
+  it('reject if no provider passed', function() {
+    try {
+      const sqr = new SqrLib();
+    } catch(e) {
+      assert.equal(e.message, 'Missing provider');
+    }
+  });
+
+  it('reject if passed wrong provider type', function() {
+    try {
+      const sqr = new SqrLib('abc');
+    } catch(e) {
+      assert.equal(e.message, 'Missing provider');
+    }
+  });
+});
 
 main.forEach(function(e) {
   describe(e.provider, function() {
