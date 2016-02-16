55af3775f5636f01ce88cc7c5d343e6ef14d1f55
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-02-16 14:38:32 +0100

    dynamic queue name

--- samples/advanced/silo/producer.js
+++ samples/advanced/silo/producer.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const shortid = require('shortid');
 const SqrLib = require('../../../lib');
 const util = require('util');
+const os = require('os');
+const path = require('path');
+const filename = path.join(os.tmpDir(), 'silo_' + shortid.generate());
 
 const data = require('./_data');
 const messages = data();
@@ -14,35 +18,39 @@ const config = {
 const provider = process.argv.slice(2).join() || 'rabbitmq';
 console.log('Using provider "' + provider + '"');
 
-const sqr = new SqrLib(provider);
+const sqr = new SqrLib(provider, {
+  silo: {
+    provider: 'nedb',
+    options: {
+      filename: filename,
+    },
+  },
+});
 
 function doSchedule() {
-  config.current++;
-  // console.log('Current ' + config.current);
-  if (config.current > messages.length - 1) {
-    console.log('No more messages to produce');
-    clearInterval(config.interval);
-    return;
-  }
+  try {
+    config.current++;
+    // console.log('Current ' + config.current);
+    if (config.current > messages.length - 1) {
+      console.log('No more messages to produce');
+      clearInterval(config.interval);
+      return;
+    }
 
-  sqr.produce({
-    queue: 'test',
-    json: {
-      key: messages[config.current],
-    },
-  }).then(function(response) {
-    console.log('response:', response);
-  }, function(err) {
-    console.error('Producer error: ');
-    console.error(util.inspect(err, {depth: 5}));
-    // clearInterval(config.interval);
-    /*
-     config.j.cancel();
-     setTimeout(function() {
-     process.exit(0);
-     }, 500);
-     */
-  });
+    sqr.produce({
+      queue: shortid.generate(),
+      json: {
+        key: messages[config.current],
+      },
+    }).then(function(response) {
+      console.log('response:', response);
+    }, function(err) {
+      console.error('Producer error: ');
+      console.error(util.inspect(err, {depth: 5}));
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
 config.interval = setInterval(doSchedule, 2000);
