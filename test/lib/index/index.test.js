6511771eb6741aa4b084f66b1403dcaa0dd63c5c
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-06-03 12:38:09 +0200

    added brick process tests

--- test/lib/index/index.test.js
+++ test/lib/index/index.test.js
@@ -2,24 +2,14 @@
 
 const o = require('../../common');
 const Context = require('events').EventEmitter;
-const context = new Context();
-let brick;
 
 describe('index / io as a brick', function() {
   it('contructor', function() {
-    brick = new o.IoBrick({}, {
+    const brick = new o.IoBrick({}, {
       name: 'cta-io',
       properties: {
         provider: {
           name: 'rabbitmq',
-          options: {},
-        },
-        start: {
-          method: 'subscribe',
-          params: {
-            key: 'key',
-            cb: function(){},
-          },
         },
       },
     });
@@ -28,6 +18,21 @@ describe('index / io as a brick', function() {
 
   it('should call provided method on start property', function(done) {
     return o.co(function* coroutine() {
+      const brick = new o.IoBrick({}, {
+        name: 'cta-io',
+        properties: {
+          provider: {
+            name: 'rabbitmq',
+          },
+          start: {
+            method: 'subscribe',
+            params: {
+              key: 'key',
+              cb: function(){},
+            },
+          },
+        },
+      });
       const subscribe = o.sinon.spy(brick.io, 'subscribe');
       yield brick.start();
       subscribe.restore();
@@ -39,15 +44,27 @@ describe('index / io as a brick', function() {
     });
   });
 
-  it.skip('should process with execution acknowledgement', function(done) {
+  it('should process with execution acknowledge', function(done) {
     return o.co(function* coroutine() {
+      const brick = new o.IoBrick({}, {
+        name: 'cta-io',
+        properties: {
+          provider: {
+            name: 'rabbitmq',
+          },
+        },
+      });
       const ack = o.sinon.stub(brick.io, 'ack', function() {
         return Promise.resolve();
       });
+      const context = new Context();
       context.data = {
         nature: {
           type: 'execution',
           quality: 'acknowledge',
+        },
+        payload: {
+          jobid: 'abc',
         },
       };
       yield brick.process(context);
@@ -55,8 +72,47 @@ describe('index / io as a brick', function() {
       o.sinon.assert.calledOnce(ack);
       done();
     })
-    .catch((err) => {
-      done(err);
-    });
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('should process with queue get', function(done) {
+    return o.co(function* coroutine() {
+      const brick = new o.IoBrick({}, {
+        name: 'cta-io',
+        properties: {
+          provider: {
+            name: 'rabbitmq',
+          },
+        },
+      });
+      const _get = o.sinon.stub(brick.io, 'get', function(queue) {
+        return Promise.resolve({
+          result: {
+            json: {
+              id: 1,
+            },
+          },
+        });
+      });
+      const context = new Context();
+      context.data = {
+        nature: {
+          type: 'queue',
+          quality: 'get',
+        },
+        payload: {
+          queue: 'abc',
+        },
+      };
+      yield brick.process(context);
+      _get.restore();
+      o.sinon.assert.calledWith(_get, 'abc');
+      done();
+    })
+      .catch((err) => {
+        done(err);
+      });
   });
 });
