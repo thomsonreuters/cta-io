36fcdb4afc62eb6d349c8c8a7175fc128d1f86de
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-06-20 10:58:30 +0200

    updated singleton test

--- test/unit/lib/io/providers/rabbitmq/index.singleton.js
+++ test/unit/lib/io/providers/rabbitmq/index.singleton.js
@@ -1,22 +1,22 @@
 'use strict';
 
 const o = require('../../../../../common');
-let two;
 
 describe('unit: rabbitmq provider singleton', function() {
   it('should return a new instance', function() {
     const one = new o.RmqProvider({newInstance: true});
     one.time = Date.now();
-    two = new o.RmqProvider({newInstance: true});
+    const two = new o.RmqProvider({newInstance: true});
     o.assert(!two.time);
   });
 
   it('should return a same instance', function() {
+    const three = new o.RmqProvider();
     const id = o.shortid.generate();
     const time = Date.now();
-    two[id] = time;
-    const three = new o.RmqProvider();
-    o.assert.property(three, id);
-    o.assert.strictEqual(three[id], time);
+    three[id] = time;
+    const four = new o.RmqProvider();
+    o.assert.property(four, id);
+    o.assert.strictEqual(four[id], time);
   });
 });
