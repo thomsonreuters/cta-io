5bedf763f0233bcbad3a6cdc44b0c9adfa0ae095
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-06-29 10:40:23 +0200

    updated recreate channel test

--- test/functional/index.channel.test.js
+++ test/functional/index.channel.test.js
@@ -3,21 +3,29 @@
 const o = require('../common');
 describe('functional: create channel retries', function() {
   it('should retry on failure', function(done) {
-    const io = new o.Io('rabbitmq', {newInstance: true});
     let create;
+    const io = new o.Io('rabbitmq', {newInstance: true});
     return o.co(function* coroutine() {
+      io.config.reChannelAfter = 200;
       // const clock = o.sinon.useFakeTimers();
       yield io._connect(false);
       create = o.sinon.stub(io.connection, 'createConfirmChannel', function(cb) {
         cb('error', null);
       });
       yield io._channel(false);
-      done();
+      create.restore();
+      done('Should not be here');
     })
     .catch((err) => {
+      console.error('err: ', err);
       create.restore();
       o.sinon.assert.calledOnce(create);
-      done();
+      create = o.sinon.spy(io.connection, 'createConfirmChannel');
+      setTimeout(function() {
+        create.restore();
+        o.sinon.assert.calledOnce(create);
+        done();
+      }, io.config.reChannelAfter);
     });
   });
 });
